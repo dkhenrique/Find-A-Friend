71b69fbab863d22a38f4ce0502eaa38150ac7e10
dkhenrique <daniel.santos998@cs.unicid.edu.br>
2026-02-28 11:10:16 -0300

    feat: Implement initial PostgreSQL database setup using Docker Compose and integrate with NestJS via TypeORM.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { PetModule } from './pet/pet.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgresConfigService } from './config/postgres.config.service';
 
 @Module({
-  imports: [UserModule, PetModule],
+  imports: [
+    UserModule,
+    PetModule,
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresConfigService,
+      inject: [PostgresConfigService],
+    }),
+  ],
 })
 export class AppModule {}
