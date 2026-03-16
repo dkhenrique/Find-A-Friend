019a882ceae7b82699008dd71858780ecc7dd95f
dkhenrique <daniel.santos998@cs.unicid.edu.br>
2026-03-16 13:03:31 -0300

    feat: Implement Pet entity and TypeORM PostgreSQL configuration for database integration.

--- src/pet/pet.entity.ts
+++ src/pet/pet.entity.ts
@@ -1,8 +1,33 @@
 import { PetEnum } from 'src/enums/petEnum';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
+@Entity({ name: 'pets' })
 export class PetEntity {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column({ name: 'name', length: 100, nullable: false })
   name: string;
+
+  @Column({ name: 'especie', enum: PetEnum, nullable: false })
   especie: PetEnum;
+
+  @Column({ name: 'adotado', nullable: false })
   adotado: boolean;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+  @DeleteDateColumn({ name: 'deleted_at' })
+  deletedAt: Date;
 }
